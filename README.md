87424518494dc02558a836e7815c9bc1b4db3a6e
ganjinrui <jinrgan@163.com>
2022-08-10 15:46:55 +0800

    module04.1: install cluster with kubeadm

--- README.md
+++ README.md
@@ -5,3 +5,30 @@
 1. 读取当前系统的环境变量中的 VERSION 配置，并写入 response header
 1. Server 端记录访问日志包括客户端 IP，HTTP 返回码，输出到 server 端的标准输出
 1. 当访问 `localhost/healthz` 时，应返回 200
+
+### kubeadm init
+
+```shell
+$ kubeadm init \
+ --image-repository registry.aliyuncs.com/google_containers \
+ --pod-network-cidr=192.168.0.0/16 \
+ --apiserver-advertise-address=192.168.172.129
+```
+
+### Copy kubeconfig
+
+```shell
+$ mkdir -p $HOME/.kube
+$ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+$ sudo chown $(id -u):$(id -g) $HOME/.kube/config
+```
+
+## Install calico cni plugin
+
+https://docs.projectcalico.org/getting-started/kubernetes/quickstart
+
+```shell
+$ kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
+$ kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
+```
+
---
+++ kubeadm-1.22.2-ubuntu-20.04/setup.sh
@@ -0,0 +1,73 @@
+DOCKER_VERSION=19.03.8
+KUBE_VERSION=1.19.3
+#/bin/sh
+yum install -y wget
+cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
+cd /etc/yum.repos.d
+wget  http://mirrors.aliyun.com/repo/Centos-7.repo
+# yum -y update
+
+# install some tools
+yum install -y vim telnet bind-utils git net-tools
+
+# open password auth for backup if ssh key doesn't work, bydefault, username=vagrant password=vagrant
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+systemctl restart sshd
+
+ntpdate 0.asia.pool.ntp.org
+
+# install docker
+yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum install -y yum-utils
+yum install -y docker-ce-"${DOCKER_VERSION}" docker-ce-cli-"${DOCKER_VERSION}" containerd.io-"${DOCKER_VERSION}"
+systemctl start docker
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "registry-mirrors": ["https://registry.cn-hangzhou.aliyuncs.com"]
+}
+EOF
+
+# Restart Docker
+systemctl daemon-reload
+systemctl restart docker
+systemctl enable docker
+
+cat >>/etc/yum.repos.d/kubernetes.repo <<EOF
+[kubernetes]
+name=Kubernetes
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
+enabled=1
+gpgcheck=0
+repo_gpgcheck=0
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+EOF
+
+setenforce 0
+
+# install kubeadm, kubectl, and kubelet.
+yum install -y kubelet-"${KUBE_VERSION}" kubeadm-"${KUBE_VERSION}" kubectl-"${KUBE_VERSION}"
+
+# Set up required sysctl params, these persist across reboots.
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sysctl --system
+
+systemctl stop firewalld
+systemctl disable firewalld
+
+swapoff -a
+# keeps the swaf off during reboot
+(crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
+sudo apt-get update -y
+
+local_ip="$(ip addr show eth1 | grep  "inet " | cut -d '/' -f 1 | awk '{print $2}')"
+cat > /etc/default/kubelet << EOF
+KUBELET_EXTRA_ARGS=--node-ip=$local_ip
+EOF
+
+systemctl enable kubelet
